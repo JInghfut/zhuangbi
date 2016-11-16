083cd0685b63324a3371e77316cbc72759cab551
root <root@MyServer352.(none)>
2016-11-16 11:46:36 +0800

    first commit

---
+++ foot.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+error_reporting(0); 
+
+?><style>
+.follow{font-size:20px;line-height:30px;color:#fff;text-align:center;padding-top:30%;z-index:2000;position:fixed;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.9);display:none;}
+.follow span{font-size:40px;position:absolute;top:10px;left:10px;}
+.follow img{width:180px;margin-top:10px;}
+</style>
+<footer class="ui-footer ui-footer-btn">
+	<ul class="ui-tiled ui-border-t">
+		<li class="ui-border-r"><a href="../"><div>更多装逼功能</div></a></li>
+		<li class="ui-border-r"><a href="http://www.duoluosb.com/"><div>装逼神器</div></a></li>
+	</ul>
+</footer>
+<script type="text/javascript">
+function show(){
+	document.getElementById("follow").style.display = "block"; 
+}
+function hide(){
+	document.getElementById("follow").style.display = "none"; 
+}
+</script>
+</body>
+</html>
